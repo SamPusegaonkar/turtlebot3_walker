a9ad2bcc101ac3fb05c02141146590b7b076b943
Sameer Pusegaonkar <sameer@umd.edu>
2021-11-25 21:03:40 -0500

    Create stub files for walker class

---
+++ include/walker.h
@@ -0,0 +1,15 @@
+/**
+ * @file walker.h
+ * @author Sameer Pusegaonkar (sameer@umd.edu)
+ * @brief A walker header file for implementing the walker methods
+ * @version 0.1
+ * @date 2021-11-25
+ * @copyright Copyright (c) 2021
+ */
+
+#ifndef INCLUDE_WALKER_H_
+
+class Walker {
+};
+
+#endif  // INCLUDE_WALKER_H_
---
+++ src/walker.cpp
@@ -0,0 +1,9 @@
+/**
+ * @file walker.cpp
+ * @author Sameer Pusegaonkar (sameer@umd.edu)
+ * @brief A walker cpp file which implmenets a walker algorithm
+ * @version 0.1
+ * @date 2021-11-25
+ * @copyright Copyright (c) 2021
+ */
+
